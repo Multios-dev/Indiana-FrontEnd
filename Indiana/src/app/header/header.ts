16c97a20b1d30482e06b1aa24c26441d12624d9c
alessiomultios <alessio.multios@gmail.com>
2026-03-25 11:14:12 +0100

    No more svg and ngif/ngfor in the code, except in the shared folder

--- Indiana/src/app/header/header.ts
+++ Indiana/src/app/header/header.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 import { ShortLanguages } from '../../enum/languages.enum';
 
+
 @Component({
   selector: 'app-header',
   imports: [CommonModule],
